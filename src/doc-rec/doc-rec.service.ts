28ce05444a90540c18a4942472733d9d707e0910
cyber4peace <LonelyAngel@mail.ru>
2022-06-06 17:23:46 +0600

    Multiple block recognition

--- src/doc-rec/doc-rec.service.ts
+++ src/doc-rec/doc-rec.service.ts
@@ -2,9 +2,22 @@ import { Injectable } from '@nestjs/common';
 
 import { createWorker, Worker } from 'tesseract.js';
 
+export interface DocumentRecognitionRectangle {
+  id: string;
+  left: number;
+  top: number;
+  width: number;
+  height: number;
+}
+
+export interface DocumentRecognitionOptions {
+  rectangles: DocumentRecognitionRectangle[];
+}
+
 interface Data {
   img: any;
   lang: string;
+  options: DocumentRecognitionOptions;
 }
 
 @Injectable()
@@ -14,6 +27,7 @@ export class DocumentRecognitionService {
   constructor() {
     this.worker = createWorker({
       logger: (data) => console.log('Worker: ', data),
+      errorHandler: (err) => console.log('Error: ', err),
     });
   }
 
@@ -26,7 +40,19 @@ export class DocumentRecognitionService {
       await this.worker.load();
       await this.worker.loadLanguage(data.lang);
       await this.worker.initialize(data.lang);
-      ret = await this.worker.recognize(buffer);
+      if (data.options?.rectangles?.length) {
+        ret = [];
+        for (const rectangle of data.options.rectangles) {
+          const rectRet = await this.worker.recognize(buffer, { rectangle });
+          ret = [ ...ret, {
+            id: rectangle.id,
+            data: rectRet.data,
+          }];
+        }
+      }
+      else {
+        ret = [ await this.worker.recognize(buffer) ];
+      }
 
       console.log('Result: ', ret);
     }
